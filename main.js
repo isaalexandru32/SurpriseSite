002595dd5f8019b7cf613ea62ddb48f0205c11c7
isaalexandru32 <116113402+isaalexandru32@users.noreply.github.com>
2024-03-05 17:59:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+let counter = document.getElementById('chair');
+let sec = 10;
+let h1Counter = document.getElementById('thing');
+let vineBoom = new Audio('vine-boom.mp3');
+let ohMahGawd = new Audio('ohmahgawd.mp3');
+
+function osmanChair() {
+    h1Counter.style.display = 'none';
+    document.body.style.backgroundImage = "url(Mikey_Yackson_Osman_chair.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    vineBoom.play();
+    ohMahGawd.play();
+    
+}
+
+
+
+function changeCounter() {
+    counter.textContent= sec;
+    sec--;
+    if (sec == -1) {
+        osmanChair();
+    }
+    else {
+        setTimeout(changeCounter, 1000)
+}
+}
+
+/* changeCounter(); */
+
+/* for (let i = 20; i >= 0; i--) {
+    document.getElementById('chair').textContent = i;
+
+} */
+
+/* document.getElementById('chair').textContent = 'doodoo'; */
